7a2b13bb5871501461d312e086eedc43059a5ac4
tariq-alhariri <tariqahmad1987@gmail.com>
2017-09-18 20:20:22 +0300

    serveImage

--- node-post-reqs/server.js
+++ node-post-reqs/server.js
@@ -1,17 +1,30 @@
 //Fill in vars here
-var 
-= /* Missing require statment */
-var 
-= /* Missing require statment */
-var 
-= /* Missing require statment */
+var http = require('http')
+
+var path = require('path');
+
+var fs =require('fs');
+
+
 
 http.createServer(function (request, response) {
  var path = url.parse(request.url, true).pathname;
 
  if (request.method === 'POST') {
+
+
+
+ 	response.writeHead(200,{'Content-Type':'text/html'});
+     fs.writeFile(__dirname + '/index.html', function(err, data){
+       if(err) console.log(err);
+       response.end(data);
+
    /*========YOUR CODE HERE=========*/
- } else if (request.method === 'GET') {
+ }
+
+
+
+  else if (request.method === 'GET') {
    if(path === '/'){
      response.writeHead(200,{'Content-Type':'text/html'});
      fs.readFile(__dirname + '/index.html', function(err, data){

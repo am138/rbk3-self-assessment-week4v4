fef405133cb4cc232ac5b014d2e31ab279e72564
unknown <dohaali425@gmail.com>
2017-09-18 20:34:34 +0300

    serveImage

--- node-post-reqs/server.js
+++ node-post-reqs/server.js
@@ -1,9 +1,9 @@
 //Fill in vars here
-var 
-= /* Missing require statment */
-var 
-= /* Missing require statment */
-var 
+var pathname=requer('requesthandler')
+/* Missing require statment */
+var path=require('path')
+ /* Missing require statment */
+var fs=require('fs')
 = /* Missing require statment */
 
 http.createServer(function (request, response) {
@@ -11,7 +11,24 @@ http.createServer(function (request, response) {
 
  if (request.method === 'POST') {
    /*========YOUR CODE HERE=========*/
- } else if (request.method === 'GET') {
+  if (path === '/highfive'){
+  	//we will write in the file 
+  	response.writeHead(200,{'Content-Type':'jpg'});
+  	  fs.writeFile(__dirname + '/highfive.jpg', function(err, data){
+       if(err) console.log(err);
+       response.end(data);
+     }
+  }
+     if(path === '/lowfive'){
+    	//we will write in the file
+    	response.writeHead(200,{'Content-Type':'jpg'});
+      fs.writeFile(__dirname + '/lowfive.jpg', function(err, data){
+       if(err) console.log(err);
+       response.end(data);
+     }
+    }
+ }
+  else if (request.method === 'GET') {
    if(path === '/'){
      response.writeHead(200,{'Content-Type':'text/html'});
      fs.readFile(__dirname + '/index.html', function(err, data){
